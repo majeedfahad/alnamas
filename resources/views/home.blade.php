ea2bc7a7430f01ee062a41180b7c1bafe83387b4
Majeed <aabdulmajeed16@gmail.com>
2023-06-25 17:53:47 +0300

    Adding style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <h2>{{ __('You are logged in!') }}</h2>
                 </div>
             </div>
         </div>
