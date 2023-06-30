a58462223c4db3dbca98d9cc3edf1882c88ec053
Abdulmajeed <aabdulmajeed16@gmail.com>
2023-07-01 00:04:06 +0300

    Adding Events Resource in filament and other changes

---
+++ app/Filament/Resources/EventResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EventResource\Pages;
+use App\Filament\Resources\EventResource\RelationManagers;
+use App\Models\Event;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class EventResource extends Resource
+{
+    protected static ?string $model = Event::class;
+
+    protected static ?string $pluralModelLabel = 'الفعاليات';
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->label('اسم الفعالية')
+                    ->required(),
+                Forms\Components\TextInput::make('description')
+                    ->label('وصفها'),
+                Forms\Components\TextInput::make('place')
+                    ->label('مكانها'),
+                Forms\Components\DateTimePicker::make('started_at')
+                    ->label('متى تبدأ'),
+                Forms\Components\SpatieMediaLibraryFileUpload::make('default')
+                    ->collection('default'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('الفعالية')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEvents::route('/'),
+            'create' => Pages\CreateEvent::route('/create'),
+            'edit' => Pages\EditEvent::route('/{record}/edit'),
+        ];
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,23 @@
         <div class="col-md-4 post-format mb-2">
             <div class="card post">
                 <div class="card-top d-flex align-items-center p-1 my-1">
-                    <img src="https://placehold.co/50x50" alt="" class="profile rounded-circle">
                     <div>
-                        <strong class="m-2">زيد عبد الرحمن</strong>
+                        <strong class="m-2">الأول من العالم في الصور أمس</strong>
+                    </div>
+                </div>
+
+                <img src="https://placehold.co/250x160" alt="" class="post-img">
+                <div class="actions p-2">
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4 post-format mb-2">
+            <div class="card post">
+                <div class="card-top d-flex align-items-center p-1 my-1">
+                    <div>
+                        <strong class="m-2">الأول من العالم في المشي أمس</strong>
                     </div>
                 </div>
 
