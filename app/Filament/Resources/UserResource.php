5d9fae9e4024a5b1d43f6479a61e68b852bb25d9
Abdulmajeed <aabdulmajeed16@gmail.com>
2023-06-30 22:41:01 +0300

    remove roles

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -26,13 +26,6 @@ class UserResource extends Resource
                 Forms\Components\TextInput::make('name')
                     ->required()
                     ->maxLength(255),
-                Forms\Components\Select::make('role')
-                    ->required()
-                    ->options([
-                        'user' => 'مستخدم',
-                        'voter' => 'مصوت',
-                        'admin' => 'مدير'
-                    ])
             ]);
     }
 
