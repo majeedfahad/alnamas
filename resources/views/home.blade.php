995b9b255b928ec83a9e6326ad60ec96a2740c87
Abdulmajeed <aabdulmajeed16@gmail.com>
2023-07-02 17:13:59 +0300

    add images if winner empty

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,26 +26,26 @@
             <div class="card post">
                 <div class="card-top d-flex align-items-center p-1 my-1">
                     <div>
-                        <h2 class="m-2">أجمل صورة أمس</h2>
+                        <h2 class="m-2">صورة أمس الفائزة</h2>
                     </div>
                 </div>
 
-                @if($image)
-                    <div class="col-md-4 mb-2">
-                        <div class="card post">
-                            <div class="card-top d-flex align-items-center p-1 my-1">
-                                <img src="{{$image->user->getFirstMediaUrl() != '' ? $image->user->getFirstMediaUrl() : 'https://placehold.co/50x50'}}"
-                                     class="profile rounded-circle best-image" style="width: 50px; height: 50px" loading="lazy"
-                                     onclick="document.getElementById('show-event-modal').src = this.src;">
-                                <div>
-                                    <strong class="m-2">{{$image->user->name}}</strong>
-                                </div>
+                <div class="col-md-4 mb-2">
+                    <div class="card post">
+                        <div class="card-top d-flex align-items-center p-1 my-1">
+                            <img src="{{$image ? $image->user->getFirstMediaUrl() : 'https://placehold.co/50x50'}}"
+                                 class="profile rounded-circle best-image" style="width: 50px; height: 50px" loading="lazy"
+                                 onclick="document.getElementById('show-event-modal').src = this.src;">
+                            <div>
+                                <strong class="m-2">{{$image->user->name ?? 'شد حيلك عشان نحط اسمك يا' . auth()->user()->name}}</strong>
                             </div>
+                        </div>
 
-                            <img src="{{$image->getFirstMediaUrl()}}" alt="" class="post-img rounded best-image" style="height: 400px;
-        object-fit: cover;"
-                                 onclick="document.getElementById('show-event-modal').src = this.src;"
-                                 loading="lazy">
+                        <img src="{{$image ? $image->getFirstMediaUrl() : asset('/img/best-image-winner.jpg')}}" alt="" class="post-img rounded best-image" style="height: 400px;
+    object-fit: cover;"
+                             onclick="document.getElementById('show-event-modal').src = this.src;"
+                             loading="lazy">
+                        @if($image)
                             <div class="actions p-2">
                                 <ul class="d-flex">
                                     <li class="p-1 m-1">
@@ -53,15 +53,9 @@
                                     </li>
                                 </ul>
                             </div>
-                        </div>
+                        @endif
                     </div>
-                @else
-                    <div class="col-md-4 mb-2">
-                        <div class="card post align-items-center mt-4">
-                            <h4>مابعد تم تحديدها</h4>
-                        </div>
-                    </div>
-                @endif
+                </div>
 
             </div>
         </div>
@@ -73,7 +67,7 @@
                     </div>
                 </div>
                 <ul class="step-places">
-                    @foreach($steps as $key => $step)
+                    @forelse($steps as $key => $step)
                         <li class="d-flex bg-light py-3 px-2 my-2 rounded-3 shadow-sm">
                             <div class="place">{{$key+1}}</div>
                             <div class="d-flex justify-content-between mx-2 align-items-center" style="width: 100%">
@@ -81,7 +75,17 @@
                                 <p class="steps-count m-0 p-0">{{$step->count}}</p>
                             </div>
                         </li>
-                    @endforeach
+                    @empty
+                        <div class="col-md-4 mb-2">
+                            <div class="card post">
+
+                                <img src="{{$image ? $image->getFirstMediaUrl() : asset('/img/walking-winner.jpg')}}" alt="" class="post-img rounded best-image" style="height: 400px;
+    object-fit: cover;"
+                                     onclick="document.getElementById('show-event-modal').src = this.src;"
+                                     loading="lazy">
+                            </div>
+                        </div>
+                    @endforelse
                 </ul>
             </div>
         </div>
